c51964fdf1d21f6140259d8e63767d2f0c7bb0f0
d2dynamicflow <devteam2@dynamicflowit.com>
2021-09-01 06:34:52 +0600

    Server Changed

--- src/Context/GlobalContext.js
+++ src/Context/GlobalContext.js
@@ -6,19 +6,19 @@ export const GlobalContext = createContext();
 export const GlobalProvider = (props) => {
   // Fetch Games
   const fetchGames = async () => {
-    const res = await fetch('http://localhost:5000/games')
+    const res = await fetch('http://51.89.149.4:5000/games')
     const data = await res.json()
     return data
   }
   // Fetch Game By Id
   const fetchGameById = async (id) => {
-    const res = await fetch(`http://localhost:5000/games/${id}`)
+    const res = await fetch(`http://51.89.149.4:5000/games/${id}`)
     const data = await res.json()
     return data
   }
   // Fetch Tournaments
   const fetchTournaments = async () => {
-    const res = await fetch('http://localhost:5000/tournaments')
+    const res = await fetch('http://51.89.149.4:5000/tournaments')
     const data = await res.json()
     return data
   }
